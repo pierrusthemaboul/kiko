a1858676d6b8b836d59c9109da18864df64bf5ed
pierre <pierre.cousin7@gmail.com>
2025-02-04 19:21:19 +0100

    test pub ok

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,7 +1,7 @@
 // app/(tabs)/index.tsx
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Image, StyleSheet, Platform, View } from 'react-native';
-import { BannerAd, BannerAdSize } from 'react-native-google-mobile-ads';
+import { BannerAd, BannerAdSize, TestIds } from 'react-native-google-mobile-ads';
 
 import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
@@ -9,6 +9,16 @@ import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 
 export default function HomeScreen() {
+  // Sélection de l'ID de test approprié selon la plateforme
+  const adUnitId = Platform.select({
+    ios: TestIds.BANNER,
+    android: TestIds.BANNER,
+  });
+
+  if (!adUnitId) {
+    return null;
+  }
+
   return (
     <ParallaxScrollView
       headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
@@ -43,7 +53,7 @@ export default function HomeScreen() {
       <ThemedView style={styles.stepContainer}>
         <ThemedText type="subtitle">Step 2: Explore</ThemedText>
         <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
+          Tap the Explore tab to learn more about what's included in this starter app. bala bala ade.
         </ThemedText>
       </ThemedView>
 
@@ -58,15 +68,13 @@ export default function HomeScreen() {
         </ThemedText>
       </ThemedView>
 
-      {/* Affichage de la bannière de test AdMob */}
       <View style={styles.adContainer}>
         <BannerAd
-          unitId="ca-app-pub-3940256099942544/6300978111" // Identifiant de test pour une bannière Android
-          size={BannerAdSize.SMART_BANNER}
-          requestOptions={{ requestNonPersonalizedAdsOnly: true }}
-          onAdFailedToLoad={(error) =>
-            console.error("Erreur lors du chargement de l'annonce :", error)
-          }
+          unitId={adUnitId}
+          size={BannerAdSize.BANNER}
+          requestOptions={{
+            requestNonPersonalizedAdsOnly: true,
+          }}
         />
       </View>
     </ParallaxScrollView>
