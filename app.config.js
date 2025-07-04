31f539c3c6e0f3f7408b1841fbe310c2dba8321b
pierrusthemaboul <pierre.cousin7@gmail.com>
2025-07-04 17:57:14 +0200

    version 100105

--- app.config.js
+++ app.config.js
@@ -37,7 +37,7 @@ export default ({ config }) => ({
       "com.google.android.gms.permission.AD_ID",
       "android.permission.ACCESS_ADSERVICES_AD_ID"
     ],
-    versionCode: 10104,
+    versionCode: 10105,
     compileSdkVersion: 35,
     targetSdkVersion: 35,
     googleServicesFile: process.env.GOOGLE_SERVICES_JSON || "./google-services.json"
